0f51bc1df66431766906000b028cfe9df6df6b0a
FullMooney <hs3235@naver.com>
2024-01-19 13:07:07 +0900

    naming

--- mq/src/test/java/com/dev/mq/SuperMoonApplicationTests.java
+++ mq/src/test/java/com/dev/mq/SuperMoonApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TistoryApplicationTests {
+class SuperMoonApplicationTests {
 
 	@Test
 	void contextLoads() {
